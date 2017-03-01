1431ba6d49e55533ba08a5b09b65e6e8e05a55f1
Splitty <splittydev@gmail.com>
2017-03-01 19:01:58 +0100

    Add gulpfile to build all projects
    Signed-off-by: Splitty <splittydev@gmail.com>

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var exec = require('child_process').exec;
+var gulp = require('gulp-help')(gulp);
+
+gulp.task('clean', 'Cleans the generated js files from lib directory', function () {
+  return del([
+    'lib/**/*'
+  ]);
+});
+
+var build_gulp_project = function (dir, next) {
+  return exec(`gulp --gulpfile ${dir}/gulpfile.js -- build`, function (err, stdout, stderr) {
+    if (err) {
+      console.log(stderr);
+    } else {
+      if (next !== void 0 && next.constructor === Function) {
+        next();
+      }
+    }
+  });
+};
+
+gulp.task('build', 'Builds all projects', [
+  'build:libarc',
+  'build:server',
+  'build:client'
+]);
+
+gulp.task('build:proto', 'Build the arc protocol', function (next) {
+  console.log('INFO: Please rebuild the protocol manually.');
+  next();
+});
+
+gulp.task('build:libarc', 'Builds the arc library', ['build:proto'], function (next) {
+  return build_gulp_project('./libarc', next);
+});
+
+gulp.task('build:server', 'Builds the arc server cli', ['build:libarc'], function (next) {
+  return build_gulp_project('./server', next);
+});
+
+gulp.task('build:client', 'Builds the arc client cli', ['build:libarc'], function (next) {
+  return build_gulp_project('./client', next);
+});
